e82f44ed1a10ac926279d175951d84dfeeb49b17
S0llerS <s0llers.official@gmail.com>
2026-01-26 00:27:13 +0200

    Menu

--- src/app/menu/[category]/page.tsx
+++ src/app/menu/[category]/page.tsx
@@ -10,19 +10,19 @@ const CategoryPage = () => {
         <Link
           href={`/product/${item.id}`}
           key={item.id}
-          className="w-full h-[60vh] border-r-2 border-b-2 border-green-500 sm:w-1/2 lg:w-1/3 p-4 flex flex-col justify-between group"
+          className="w-full h-[60vh] border-r-2 border-b-2 border-green-500 sm:w-1/2 lg:w-1/3 p-4 flex flex-col justify-between group hover:bg-lime-50 transition-all duration-200"
         >
           {/* IMAGE CONTAINER */}
           {item.img && (
             <div className="relative h-[80%]">
-              <Image src={item.img} alt="" fill className="object-contain" />
+              <Image src={item.img} alt="" fill className="object-contain hover:rotate-3 transition-all duration-400" />
             </div>
           )}
           {/* TEXT CONTAINER */}
           <div className="flex items-center justify-between font-bold">
             <h1 className="text-2xl uppercase p-2">{item.title}</h1>
-            <h2>€{item.price}</h2>
-            <button className="hidden group-hover:block uppercase bg-green-500 text-white p-2 rounded-md">
+            <h2 className="group-hover:hidden text-xl">€{item.price}</h2>
+            <button className="hidden group-hover:block uppercase bg-green-500 text-white p-2 rounded-md cursor-pointer hover:scale-105 hover:rotate-2 transition-all duration-200">
               Add to Cart
             </button>
           </div>
