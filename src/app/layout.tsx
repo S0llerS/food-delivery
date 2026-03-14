e199620d9b0f83bf82156c56afaf734382c924fe
S0llerS <s0llers.official@gmail.com>
2026-03-15 01:03:42 +0200

    Version Match and Fixes!
    - downgraded most of the stuff
    - cleaned up
    - fixed Prisma client not working
    - broke Tailwind CSS
    - fixed Tailwind CSS
    
    W?

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,18 +3,18 @@ import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
 
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+//import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
+// const geistSans = Geist({
+//   variable: "--font-geist-sans",
+//   subsets: ["latin"],
+// });
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+// const geistMono = Geist_Mono({
+//   variable: "--font-geist-mono",
+//   subsets: ["latin"],
+// });
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -28,9 +28,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+      <body className={`antialiased`}>
         <Notification />
         <Navbar />
         {children}
